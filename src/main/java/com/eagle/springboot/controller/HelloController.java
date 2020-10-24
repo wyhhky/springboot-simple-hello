05a122198c67c79433a1110724261738afabac69
wyhhky <60504940+wyhhky@users.noreply.github.com>
2020-10-24 20:12:21 +0800

    Update HelloController.java
    Add Port monitoring

--- src/main/java/com/eagle/springboot/controller/HelloController.java
+++ src/main/java/com/eagle/springboot/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.eagle.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    String port;
+
     @GetMapping("/hello")
     public String sayHello(){
-        return "Simple Hello!";
+        return "Simple Hello! The port is "+port;
     }
 }
