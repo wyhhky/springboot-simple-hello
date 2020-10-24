c8ec36a0ac7a5b669e63f202a00ab3a82b721f4d
wyhhky <60504940+wyhhky@users.noreply.github.com>
2020-10-24 17:13:38 +0800

    Add files via upload
    Simple Hello springboot sample

---
+++ src/main/java/com/eagle/springboot/SpringbootSimpleHelloApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleHelloApplication.class, args);
+	}
+
+}
