6be64d9e952ad9a24b23bb16c544152fe853086f
wyhhky <60504940+wyhhky@users.noreply.github.com>
2020-10-24 21:46:40 +0800

    Update HelloController.java
    Added datetime output

--- src/main/java/com/eagle/springboot/controller/HelloController.java
+++ src/main/java/com/eagle/springboot/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.eagle.springboot.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello(){
+        System.out.println("date:" + new Date());
         return "Simple Hello! The port is "+port;
     }
 }
